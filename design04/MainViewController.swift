e1a51ae6e16eac39d9bda4c202b0cb5adfa5eefc
Leandro Hernandez <leandro.hdez@hotmail.com>
2015-09-17 15:20:19 +0200

    Development progress
    - Parallax effect in header image.

---
+++ design04/MainViewController.swift
@@ -0,0 +1,129 @@
+//
+//  MainViewController.swift
+//  design04
+//
+//  Created by IT-ELEVEN on 17/9/15.
+//  Copyright (c) 2015 swiftcombat. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController, UIScrollViewDelegate {
+
+    // constantes 
+    let constHeaderHeight: CGFloat = 250
+    let constNavBarHeight: CGFloat = 64
+    
+    @IBOutlet var headerView: UIView?
+    @IBOutlet var headerImage: UIImageView?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationBarVisibility(false)
+        
+        // Ademas:
+        // activar la propiedad del TableViewController (Under Top Bars) en el StoryBoard
+        // desactivar la propiedad del TableViewController (Layout, Adjunt Scroll View Controller) en el StoryBoard
+        self.view.sendSubviewToBack(self.headerView!)
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.headerView!.frame = CGRectMake(0, 0, CGRectGetWidth(self.tableView.frame), self.constHeaderHeight)
+        
+        self.headerImage?.image = imageWithImage(UIImage(named: "wood")!, CGSizeMake(CGRectGetWidth(self.headerView!.frame), self.constHeaderHeight))
+    }
+
+
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseCellIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+    
+    // MARK: -
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        var scrollOffsetY = self.constHeaderHeight - scrollView.contentOffset.y
+        
+        // stretch the header view if neccessary
+        if scrollOffsetY > self.constHeaderHeight {
+            // stretch
+            self.headerImage!.contentMode = UIViewContentMode.ScaleAspectFill
+            self.headerTopLayoutConstraint()?.constant = self.constHeaderHeight - scrollOffsetY
+        }
+        else {
+            // scroll
+            self.headerImage!.contentMode = UIViewContentMode.Center
+            self.headerTopLayoutConstraint()?.constant = -(self.constHeaderHeight - scrollOffsetY)
+            self.headerBootomLayoutConstraint()?.constant = -2*(self.constHeaderHeight - scrollOffsetY)
+        }
+        
+        // show or hide the navigation bar
+        if scrollOffsetY <= self.constNavBarHeight {
+            self.navigationBarVisibility(true)
+        }
+        else{
+            self.navigationBarVisibility(false)
+        }
+    }
+    
+    
+    // MARK: -
+    func headerTopLayoutConstraint() -> NSLayoutConstraint? {
+        for layout in self.headerView!.constraints() {
+            if (layout as! NSLayoutConstraint).firstAttribute == NSLayoutAttribute.Top {
+                return layout as? NSLayoutConstraint
+            }
+        }
+        return nil
+    }
+    
+    func headerBootomLayoutConstraint() -> NSLayoutConstraint? {
+        for layout in self.headerView!.constraints() {
+            if (layout as! NSLayoutConstraint).firstAttribute == NSLayoutAttribute.Bottom {
+                return layout as? NSLayoutConstraint
+            }
+        }
+        return nil
+    }
+    
+    
+    // MARK: -
+    func navigationBarVisibility(visible: Bool) {
+        if visible == true {
+            self.navigationController?.navigationBar.setBackgroundImage(ImageWithColor(UIColor.whiteColor()), forBarMetrics: UIBarMetrics.Default)
+            self.navigationController?.navigationBar.shadowImage = ImageWithColor(UIColor.darkGrayColor().colorWithAlphaComponent(0.4), CGSizeMake(0.5, 0.5))
+            self.navigationController?.navigationBar.translucent = true
+        }
+        else{
+            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+            self.navigationController?.navigationBar.translucent = true
+        }
+    }
+    
+    // MARK: -
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
